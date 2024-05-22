e83a6aa834bc7e8bd836101ce49126d87a102e9b
jenifertorresnavarro <jenifertorresnavarro@gmail.com>
2024-05-21 19:56:31 -0700

    get, post, put, delete, for all posts

---
+++ routes/api-routes/thought-routes.js
@@ -0,0 +1,25 @@
+// importing dependancies 
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThought,
+    deleteThought,
+    updateThoughtById,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+// routes for GET and POST all Thoughts
+router.route('/').get(getAllThoughts).post(createThought);
+
+// routes for GET, PUT and DELETE Thoughts
+router.route('/:thoughtId').get(getThoughtsById).put(updateThoughtById).delete(deleteThought);
+
+// route for POST reaction to a Thought
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// route for DELETE reaction to a Thought
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
